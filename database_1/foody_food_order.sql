12acde8c3d50783a2c61315425aef783f8bf9ba2
Nguyễn Thanh Tịnh <142908230+MrToInh@users.noreply.github.com>
2024-03-21 23:45:49 +0700

    first commit db

---
+++ database_1/foody_food_order.sql
@@ -0,0 +1,70 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: foody
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `food_order`
+--
+
+DROP TABLE IF EXISTS `food_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `food_order` (
+  `id` int NOT NULL,
+  `customer_id` int NOT NULL,
+  `restaurant_id` int NOT NULL,
+  `customer_address_id` int NOT NULL,
+  `order_status_id` int NOT NULL,
+  `assigned_driver_id` int NOT NULL,
+  `order_datetime` datetime NOT NULL,
+  `delivery_fee` float NOT NULL,
+  `total_amount` varchar(45) NOT NULL,
+  `requested_delivery_datetime` datetime NOT NULL,
+  `cust_driver_rating` varchar(45) NOT NULL,
+  `cust_restaurant_rating` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `restaurant_id` (`restaurant_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `assigned_driver_id` (`assigned_driver_id`),
+  KEY `customer_address_id` (`customer_address_id`),
+  KEY `order_status_id` (`order_status_id`),
+  CONSTRAINT `food_order_ibfk_1` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`),
+  CONSTRAINT `food_order_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `food_order_ibfk_3` FOREIGN KEY (`assigned_driver_id`) REFERENCES `delivery_driver` (`id`),
+  CONSTRAINT `food_order_ibfk_4` FOREIGN KEY (`customer_address_id`) REFERENCES `customer_address` (`id`),
+  CONSTRAINT `food_order_ibfk_5` FOREIGN KEY (`order_status_id`) REFERENCES `order_status` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food_order`
+--
+
+LOCK TABLES `food_order` WRITE;
+/*!40000 ALTER TABLE `food_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `food_order` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-21 23:45:00
