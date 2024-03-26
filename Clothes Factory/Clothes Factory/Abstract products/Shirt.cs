a3fa81ed57dd042b085ea15426172a590a065ea6
Ngocmanh1607 <ngocmanh160703@gmail.com>
2024-03-27 00:18:19 +0700

    demo

---
+++ Clothes Factory/Clothes Factory/Abstract products/Shirt.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace Clothes_Factory.Abstract_products
+{
+    abstract class Shirt
+    {
+        public abstract void showShirt(PictureBox pictureBox);
+    }
+}
