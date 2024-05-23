94e9047e2fd7480ed30a1b0c070dbd22b65dd07d
BDang029 <dodang159@gmail.com>
2024-05-23 20:21:13 +0700

    lala

--- webapp/BackEnd/app/routes/user/order.routes.js
+++ webapp/BackEnd/app/routes/user/order.routes.js
@@ -6,7 +6,7 @@ module.exports = app => {
     const Orderdetails = require("../../controllers/user/orderdetail.controller.js");
     const Order = require("../../controllers/user/order.controller.js");
     const router = require("express").Router();
-    router.post("/addorder",verifyToken, Orderdetails.OrderInProcess);
+    router.post("/addorder",Orderdetails.OrderInProcess);
     router.get("/getorder",verifyToken, Order.getAllOrderProcessing);
     app.use("/api/orders", router);
 }
