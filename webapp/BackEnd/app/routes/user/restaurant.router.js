01f0ec7690cdd65f8bc905b6679b5d7d80f24a71
BDang029 <dodang159@gmail.com>
2024-05-23 10:42:12 +0700

    update res

--- webapp/BackEnd/app/routes/user/restaurant.router.js
+++ webapp/BackEnd/app/routes/user/restaurant.router.js
@@ -4,7 +4,7 @@ module.exports=app=>{
     const restaurant_controller=require('../../controllers/user/restaurant.controller.js');
     const router=require('express').Router();
     router.post('/',verifyToken,restaurant_controller.addrestaurant);
-    router.get('/item',restaurant_controller.getRestaurant);
-    router.get('/',restaurant_controller.getAllRestaurants);
+    router.get('/',restaurant_controller.getRestaurant);
+    router.get('/getall',restaurant_controller.getAllRestaurants);
     app.use('/api/restaurants',router);
 }
